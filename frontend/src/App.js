26e14f352e564cd7e58b5f794a315af816ac56ce
Shivkamp <132839557+Shivkamp@users.noreply.github.com>
2024-08-28 23:35:31 +0530

    button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,12 @@ function App() {
   return (
     <BrowserRouter>
       <AppStyled className="App">
+        <h1>Video Library</h1>
+        <Routes>
+          <Route path='/' element={<Landing />} /> {/* Set Landing as the default page */}
+          <Route path='/videos' element={<Videos />} /> {/* Video Library route */}
+          <Route path='/videos/:id' element={<VideoPlayer />} /> {/* Video Player route */}
+        </Routes>
         <div className="upload">
           <Button 
             name="Upload"
@@ -22,12 +28,6 @@ function App() {
           />
         </div>
         {modal && <Upload />}
-        <h1>Video Library</h1>
-        <Routes>
-          <Route path='/' element={<Landing />} /> {/* Set Landing as the default page */}
-          <Route path='/videos' element={<Videos />} /> {/* Video Library route */}
-          <Route path='/videos/:id' element={<VideoPlayer />} /> {/* Video Player route */}
-        </Routes>
         {modal && <div className="overlay" onClick={() => setModal(false)}></div>}
       </AppStyled>
     </BrowserRouter>
@@ -36,6 +36,9 @@ function App() {
 
 const AppStyled = styled.div`
   padding: 3rem 18rem;
+  position: relative;
+  min-height: 100vh;  /* Ensure full height to place the button at the bottom */
+
   h1 {
     color: #fff;
     background: black;
@@ -47,6 +50,16 @@ const AppStyled = styled.div`
     transform: translateX(-50%);
   }
 
+  .upload {
+    position: absolute;
+    bottom: 20px;  /* Adjust the value to control distance from the bottom */
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+
   .overlay {
     position: fixed;
     top: 0;
@@ -54,11 +67,6 @@ const AppStyled = styled.div`
     width: 100%;
     height: 100%;
     background: rgba(0,0,0,0.5);
-  }
-  
-  .upload {
-    display: flex;
-    justify-content: flex-start;
   }
 `;
 
